65bc8f44be14cc7e46ebe000bc0f93c9240553d6
Renan <renanpellegrini@outlook.com>
2023-06-19 16:19:51 -0300

    adicionando seções

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
     const buttons = document.querySelectorAll('[data-tab-button]')
     const tabsContainer = document.querySelectorAll('[data-tab-id]')
+    const questions = document.querySelectorAll('[data-faq-question]')
     
     for (let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botão){
@@ -12,7 +13,18 @@ document.addEventListener("DOMContentLoaded", function(){
             botão.target.classList.add('shows__tabs__button--is-active')
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreFechaResposta)
+    }
 })
+
+function abreFechaResposta (elemento) {
+    const classe = 'faq__questions__item--is-open'
+    const elementoPai = elemento.target.parentNode
+
+    elementoPai.classList.toggle(classe)
+}
 
 function removeBtnAtivo () {
     const buttons = document.querySelectorAll('[data-tab-button]')
